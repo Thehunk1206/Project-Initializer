7f408b1276798502fcf4af206412eb115404d9e5
predatorx7 <smushaheed@outlook.com>
2019-07-03 13:22:57 +0530

    fixed critical bugs.

--- bin/createproject.py
+++ bin/createproject.py
@@ -1,61 +1,48 @@
 import os
-import sys
 from selenium import webdriver
 import subprocess
 import time
 import urllib
+from sys import argv, platform
 
-# kept in global scope for easy access
-if len(sys.argv) != 4:
-    print('Input error.')
-else:
+def createDirectory():
+    global projectPath
+    if len(argv) != 4:
+        print("Didn\'t get expected inputs from sysarg")
+        return
     # This sets path for project folder
-    projectPath = os.path.join(os.path.expanduser("~"), 'project-initializer')
+    # Application will save projects to ~/project-initializer/projects by default
+    projectPath = os.path.join(os.path.expanduser(
+        "~"), 'project-initializer', 'projects')
     if not os.path.exists(projectPath):
         os.mkdir(projectPath)
     else:
         print("Directory ", projectPath,  " already exists")
 
-    try:
-        # Github login information goes here
-        # Username
-        USERNAME = str(sys.argv[2])
-        # Password 'WARNING:Do not share this piece of scirpt to any one with Password Entered'
-        PASSWORD = str(sys.argv[3])
-    else:
-        # lol
-        print("Username \& Password input error.")
+
+def WebdriverDo():
     # Asking User to enter the description of project which has to be in Github project description
     Description = str(input("Enter the description of project: "))
 
-    # Setting the path of chrome driver
-    ## EXAMPLE: '/usr/lib/chromedriver_linux64/chromedriver'
-    # Default is lib/chromedriver of project path
-    browser = webdriver.Chrome(executable_path=os.path.join(
-        projectPath, 'lib', 'chromedriver'))
+    # Give your chromedriver's executable path as an argument to parameter "executable_path"
+    ## EXAMPLE: browser = webdriver.Chrome(executable_path='/usr/lib/chromedriver_linux64/chromedriver')
+    browser = webdriver.Chrome(
+        executable_path='/usr/lib/chromedriver_linux64/chromedriver')
+    # Also, you could use firefox's geckodriver. We used chromedriver above.
     browser.get("https://github.com/login")
 
-
-def create():
-    global Description, USERNAME, PASSWORD
-
-    if sys.argv[1] == None:
-        print("Usage: createproject <project name>")
-    else:
-        folderName = str(sys.argv[1])
-        print(folderName)
-        os.makedirs(projectPath + folderName)
-
     username = browser.find_element_by_name('login')
-    username.send_keys(USERNAME)
+    # GithubUsername from sysarg are passed here
+    username.send_keys(str(argv[2]))
 
     password = browser.find_element_by_name('password')
-    password.send_keys(PASSWORD)
+    # GithubPassword from sysarg are passed here
+    password.send_keys(str(argv[3]))
 
     loginbutton = browser.find_element_by_name('commit')
     loginbutton.click()
 
-    print(USERNAME, " logged in successfully!")
+    print(str(argv[3]), " logged in successfully!")
 
     browser.get("https://github.com/new")
     reponame = browser.find_element_by_name("repository[name]")
@@ -69,7 +56,33 @@ def create():
 
     time.sleep(3)
     browser.close()
+    makeLocal()
+
+
+def makeLocal():
+    global projectPath
+    if argv[1] == None:
+        print("Usage: createproject <project-name>")
+    else:
+        print(folderName)
+        projectSaveDirectory = os.path.join(projectPath, folderName)
+        os.makedirs(projectSaveDirectory)
+        print("Project will be saved at ", projectSaveDirectory)
+        print("Making", folderName, " locally available")
+    gitCommands = ("git init",
+                   "git remote add origin https://github.com/"+str(argv[2])+"/"+folderName,
+                   "touch README.md",
+                   "git add .",
+                   "git commit -m 'first commit'",
+                   "git push -u origin master",
+                   "exec bash")
+    for i in range(7):
+        os.system(gitCommands[i])
+    pass
 
 
 if __name__ == "__main__":
-    create()
+    # python interpreter is running this file as the main program, thus _name_=="__main__"
+    projectPath, folderName = "", str(argv[1])
+    createDirectory()
+    WebdriverDo()
