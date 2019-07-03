81b15c2deb776cab9b1bc5515f8c63731cfab4e7
predatorx7 <smushaheed@outlook.com>
2019-07-03 13:58:01 +0530

    Making functional for Windows, STATUS: NOT TESTED

---
+++ bin/createproject.ps1
@@ -0,0 +1,14 @@
+# DONT RUN
+function createproject($githubProjectName) {
+    $usernameGithub = Read-Host "Enter Your Github Username: "
+    Write-Host
+    $usernamePassword = Read-Host "Enter Your Github Password: " -AsSecureString
+    Write-Host
+    if ([string]::IsNullOrWhitespace($githubProjectName)){
+        $githubProjectName =  Read-Host "Enter project name: "
+    }
+    Write-Host "Creating project $githubProjectName ..."
+    python3 'createproject.py' $githubProjectName $usernameGithub $usernamePassword
+}
+
+createproject $githubProjectName
